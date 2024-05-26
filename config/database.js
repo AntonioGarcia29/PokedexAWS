a316607b546dc5139c509203a86ec895513e84ae
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2024-05-26 01:10:49 -0600

    xd

--- config/database.js
+++ config/database.js
@@ -1,23 +1,19 @@
+const mysql = require('mysql');
 require('dotenv').config();
 
-const mysql = require('mysql');
-const util = require('util');
-
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-    if (err) {
-      console.error('Error connecting to the database:', err);
-      return;
-    }
-    console.log('Connected to the database.');
-  });
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    process.exit(1); // Salir del proceso si no se puede conectar a la base de datos
+  }
+  console.log('Connected to the database.');
+});
 
-pool.query = util.promisify(pool.query);
-module.exports = pool;
+module.exports = connection;
