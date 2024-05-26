c05b48c683ba85b0ce78db81e2443816cfe79c78
Antonio Iván García Galván <agarcia261@alumnos.uaq.mx>
2024-05-26 01:01:55 -0600

    resemos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 require('dotenv').config(); 
+const path = require('path')
 
 // routers
 const pokedex = require('./routes/pokedex');
@@ -10,15 +11,18 @@ const pokedex = require('./routes/pokedex');
 const cors = require('./middleware/cors');
 const index = require('./middleware/index');
 const notFound = require('./middleware/notFound');
+app.use(express.static(path.join(__dirname, 'interfaz')));
 
 app.use(cors);
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/", index); 
+app.get("/",(request, response)=>{
+    response.sendFile(path.resolve(__dirname,'index.html'))
+} ); 
 
 app.use('/pokedex', pokedex); 
-app.use(express.static('public'));
 
 app.use(notFound)
 
